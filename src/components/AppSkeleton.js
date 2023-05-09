89d67ef138f6d895c73717994d6481ef25e5fbf4
Pankaj Kumar <805.bluebell@gmail.com>
2023-05-09 17:58:43 +0530

    added new app skeleton

---
+++ src/components/AppSkeleton.js
@@ -0,0 +1,23 @@
+export default function AppSkeleton() {
+  return (
+    <div className="">
+      <span class="flex flex-row m-8">
+        <span>Brand</span>
+      </span>
+      <span className="flex flex-row-reverse space-x-4">
+        <span>Help</span>
+        <span>Options</span>
+        <span>pranav sethi</span>
+      </span>
+
+      <span class="flex flex-col space-y-8">
+        <span>Accounts</span>
+        <span>Campaigns</span>
+        <span>Analytics</span>
+        <span>Inbox</span>
+        <span>Settings</span>
+        <span>Accelerator</span>
+      </span>
+    </div>
+  );
+}
--- src/pages/accounts.js
+++ src/pages/accounts.js
@@ -1,3 +1,14 @@
+import AppSkeleton from "@/components/AppSkeleton";
+
+function AccountsContent() {
+  return <div>Accounts tab</div>;
+}
+
 export default function Accounts() {
-  return <main>Accounts</main>;
+  return (
+    <main>
+      <AppSkeleton />
+      <AccountsContent />
+    </main>
+  );
 }
